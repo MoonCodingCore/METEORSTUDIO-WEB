7128e06188b440cdb5a22de6522fff7bd85f03f9
myclass <myclxss@outlook.com>
2023-12-29 01:28:19 -0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
-(function () {
+/**
+* Template Name: Tempo
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/tempo-free-onepage-bootstrap-theme/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
   "use strict";
+
+  /**
+   * Easy selector helper function
+   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -8,18 +19,44 @@
       return document.querySelector(el)
     }
   }
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+  window.addEventListener('load', function () {
+    var loader = document.getElementById('loader');
+    setTimeout(function () {
+      loader.classList.add('fadeOut');
+    }, 100);
+  });
 
+
+  /**
+   * Easy event listener function
+   */
   const on = (type, el, listener, all = false) => {
-    if (all) {
-      select(el, all).forEach(e => e.addEventListener(type, listener))
-    } else {
-      select(el, all).addEventListener(type, listener)
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
     }
   }
 
+  /**
+   * Easy on scroll event listener 
+   */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
+
+  /**
+   * Navbar links active state on scroll
+   */
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -36,12 +73,16 @@
   }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
 
     if (!header.classList.contains('header-scrolled')) {
-      offset -= 10
+      offset -= 16
     }
 
     let elementPos = select(el).offsetTop
@@ -51,6 +92,9 @@
     })
   }
 
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
@@ -63,13 +107,46 @@
     window.addEventListener('load', headerScrolled)
     onscroll(document, headerScrolled)
   }
-  on('click', '.mobile-nav-toggle', function (e) {
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  on('click', '.scrollto', function (e) {
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
 
@@ -83,28 +160,77 @@
       scrollto(this.hash)
     }
   }, true)
+
+  /**
+   * Porfolio isotope and filter
+   */
   window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
     }
   });
-  const portfolioLightbox = GLightbox({
-    selector: '.portfokio-lightbox'
-  });
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
   function aos_init() {
     AOS.init({
       duration: 1000,
-      easing: "ease-in-out",
+      easing: 'ease-in-out',
       once: true,
       mirror: false
     });
   }
   window.addEventListener('load', () => {
     aos_init();
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
   });
-
-  new PureCounter();
-
-})();
+})()
